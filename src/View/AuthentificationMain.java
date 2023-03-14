5f028c67a8cdd818a3e75ae0b17a3f70d7e63578
merrymerrykate <55756069+merrymerrykate@users.noreply.github.com>
2023-03-14 23:20:31 +0300

    Add files via upload

---
+++ src/View/AuthentificationMain.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Set;
+
+/**
+ *
+ * @author kateromanova
+ */
+public class AuthentificationMain extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent gp = FXMLLoader.load(getClass().getResource("FXMLAuthentication.fxml"));
+
+        Scene scene = new Scene(gp,400,300);
+        stage.setTitle("Welcome");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+
+    }
+}
